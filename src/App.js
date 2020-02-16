ee50c0a2aeada8ca2abb944ca6114971564f8897
erishdaren <moreno240190@gmail.com>
2020-02-15 22:15:54 -0600

    Proyecto Nuevo

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendiendo React en Tabasco
         </a>
       </header>
     </div>
