ec492156af0c38c9f2fdeef7174ec1586ccb2c24
erishdaren <moreno240190@gmail.com>
2020-02-18 13:01:10 -0600

    Guardar datos en el State con el evento onSubmit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendiendo React en Tabasco
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+export default class App extends Component {
+  state = {
+    nombre: "Erick Moreno Hidalgo",
+    name: "",
+    cel: ""
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  handleChange = e => {
+    this.setState({});
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            El estudiante: <strong>{this.state.nombre}</strong>, esta
+            aprendiendo React en Tabasco
+          </p>
+          <form onSubmit={this.handleSubmit}>
+            <p>
+              <label htmlFor="titulo">Nombre:</label>
+              <input
+                id="name"
+                name="userName"
+                placeholder="ingresa el nombre"
+                onChange={e => this.setState({ name: e.target.value })}
+              />
+            </p>
+            <p>
+              <label htmlFor="titulo">Telefono:</label>
+              <input
+                id="cel"
+                name="userCel"
+                placeholder="Ingresa tu numero telefonico"
+                onChange={e => this.setState({ cel: e.target.value })}
+              />
+            </p>
+            <button>Enviar</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+let a = 'text';
+
+export default class Class extends Component {
+
+    render() {
+        return (
+            <div className="class-name">
+                content
+            </div>
+        );
+    }
+}
